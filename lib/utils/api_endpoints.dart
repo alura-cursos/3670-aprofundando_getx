bd92e53a62e08ec662ab715c6304366e5b949a0d
ikyrie <mah.c5r@gmail.com>
2024-03-06 11:09:13 -0300

    video1.4

---
+++ lib/utils/api_endpoints.dart
@@ -0,0 +1,7 @@
+class ApiEndpoints {
+  static const String baseUrl = "http://localhost:3000/";
+  static const String register = "register";
+  static const String login = "login";
+  static const String videos = "videos";
+  static const String addVideoByUser = "600/videos";
+}
