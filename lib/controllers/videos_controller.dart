00eac3cd3f4842038b8bf489d4769a920a31a538
ikyrie <mah.c5r@gmail.com>
2024-03-08 12:00:58 -0300

    video3.4

--- lib/controllers/videos_controller.dart
+++ lib/controllers/videos_controller.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vidflow/model/video.dart';
+import 'package:vidflow/screens/dashboard.dart';
 import 'package:vidflow/services/videos_api.dart';
+import 'package:vidflow/utils/snackbars.dart';
 
 class VideosController extends GetxController {
   final videoLoading = true.obs;
   final RxList<Video> videos = <Video>[].obs;
   final VideosApi videosApi = Get.put(VideosApi());
+
+  final TextEditingController textVideoTitleController = TextEditingController();
+  final TextEditingController textVideoThumbNailController = TextEditingController();
 
   Future<void> getAllFromUser() async {
     try {
@@ -16,6 +22,23 @@ class VideosController extends GetxController {
       }
     } finally {
       videoLoading(false);
+    }
+  }
+
+  Future<void> create() async {
+    final Video video = Video(thumbURL: textVideoThumbNailController.text, title: textVideoTitleController.text, userId: 2);
+    try {
+      Response<void> response = await videosApi.create(video, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hdGhldXNAZW1haWwuY29tIiwiaWF0IjoxNzA5OTA5ODE1LCJleHAiOjE3MDk5MTM0MTUsInN1YiI6IjIifQ.HnITLXphn0BLsepoQVvmFwgeCaE5ck4-uwnj9PNDkkw");
+      if(response.statusCode == 201) {
+        AppSnacks.getSuccessUpload();
+        Get.offAll(() => Dashboard());
+        getAllFromUser();
+      } else {
+        AppSnacks.getErrorUpload();
+        throw response.statusText!;
+      }
+    } catch (e) {
+      print(e);
     }
   }
 }
